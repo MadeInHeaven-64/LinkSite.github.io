289f49168ad684a7ac5e2bbf0e1cee41882b5d95
theonlyshannon <sofwannuhaalfaruq@gmail.com>
2023-11-19 17:51:33 +0700

    Second Initial Commit (before update)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color1: "#176B87",
+        color2: "#64CCC5",
+        color3: "#DAFFFB",
+        dark: "#001C30",
+      }
+    },
   },
   plugins: [],
 }
